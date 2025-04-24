c76f688022f2d411c43e45db3851aaab02b6b84d
jaspreet0310 <jas031092@gmail.com>
2025-04-24 22:37:15 +0530

    Add files via upload

---
+++ assignments.py
@@ -0,0 +1,16 @@
+#Task-1
+'''
+a=float(a)
+b=input('b: ')
+b=float(b)
+c=a+b
+print('a+b= ',c)
+d=a*b
+print('a*b=', d)
+e=b/a
+print('a/b= ',e)
+f=b-a
+print('b-a=', f)
+'''
+
+
---
+++ task 2.py
@@ -0,0 +1,5 @@
+#Task-2
+first_name=input('first name: ')
+last_name=input('last name: ')
+c=first_name+' '+last_name
+print(f"\nHello, {c}! Welcome to the Python program.")
